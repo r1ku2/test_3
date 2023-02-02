2a969180a93854f8cbb8ebfb895d5f4f5c10d4dd
r1ku2 <123049674+r1ku2@users.noreply.github.com>
2023-02-02 19:42:02 +0900

    Add files via upload

---
+++ python_test_hoge.py
@@ -0,0 +1,20 @@
+import random
+
+# 疑似乱数ジェネレータを初期化して乱数シードを固定
+random.seed(1)
+
+# 1～10の乱数を3個生成
+for k in range(3):
+    x = random.random()
+    print(x)
+
+# １行開ける
+print("")
+
+# 疑似乱数ジェネレータを初期化して乱数シードを固定
+random.seed(1)
+
+# 1～10の乱数を3個生成
+for k in range(3):
+    x = random.random()
+    print(x)
